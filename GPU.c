448a2d80af9b3b44cc79f9ba5a8c279e1362f032
Sal <salvatoreaamico@gmail.com>
2018-02-13 18:08:24 -0500

    Updated Description
    Updated GPU Description to see if push works for SalvatoreAAmico account.

--- GPU.c
+++ GPU.c
@@ -1,7 +1,12 @@
 /* 
-Lucas Tata / Salvatore Amico
-High Performance Computing on GPUs
-Final Project
-Collaborative Filtering
-GPU Implementation
+Created By: Lucas Tata / Salvatore Amico
+Created On: 13 February 2018
+Created To: High Performance Computing on GPUs
+			Final Project
+			Collaborative Filtering
+			GPU Implementation
+
+Modified On: 13 February 2018
+Modified By: Salvatore Andrew Amico (SAA)
+Modified To: See if Push Works
 */
