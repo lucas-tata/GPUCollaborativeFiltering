2dac1b0020aebeb6e10828e4db49eb2675b4a0cc
lucas-tata <lucas_tata@student.uml.edu>
2018-02-28 19:59:24 -0500

    Starting to Parse Dataset

--- CPU.c
+++ CPU.c
@@ -5,3 +5,58 @@ Final Project
 Collaborative Filtering
 CPU Implementation
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+int main (int args, char **argv)
+{
+
+	FILE* data = fopen("usersha1-artmbid-artname-plays.tsv", "r"); //our dataset file (tab separated file)
+	if(data == NULL)
+	{
+		printf("File read error");
+		return 0;
+	}
+
+	char dataLine[1024]; //where we will store each line of the data read
+	char * dataMatrix[1024][3]; //our output matrix we are storing in (basically a 2d string array)
+	int i = 0, j = 0;
+    while (1)
+    {
+        if(i < 1024 && fgets(dataLine, sizeof(dataLine), data) != NULL) //reading in 1024 lines using fgets and putting it in dataLine
+		{
+			char * token = strtok(dataLine, "\t"); //parsing the data with the tab separater
+			
+			j = 0;
+			while(j < 3) {
+				
+				dataMatrix[i][j] = token;
+				//printf( " %s\n", dataMatrix[i][j] );
+				token = strtok(NULL, "\t"); //reading the next value of the parsed data
+				if (token == NULL) //if there isnt a token, break out
+				break;
+				//printf("%s ", token);
+				j++;
+			}
+
+			i++;
+		}
+		else
+		{
+			break;
+		}
+    }
+	for(i = 0; i < 1024; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			printf("%s\n ", dataMatrix[i][j]); //as you can see from the print statements, data isn't being stored correctly
+		}
+	}
+	
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GPUCollaborativeFiltering
 Luke and Sal's HPC GPU Project
+
+The main dataset used for this project is located here: 
+http://www.dtic.upf.edu/~ocelma/MusicRecommendationDataset/lastfm-360K.html
+The name of the dataset file is "usersha1-artmbid-artname-plays.tsv"
+The file should be in the SAME directory as the code files
